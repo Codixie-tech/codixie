e844eab7dae6df1c321aa7a538be600681e224b1
Volkov Dmitry <vdmitry.log@gmail.com>
2026-05-21 01:07:27 +0300

    fix import fuctionality

--- src/shared/contracts.ts
+++ src/shared/contracts.ts
@@ -4,39 +4,49 @@ export const TagDTOSchema = z.object({
   id: z.string().uuid(),
   name: z.string(),
   color: z.string(),
-  deletedAt: z.string().nullable(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
+  deletedAt: z.coerce.string().nullable(),
+  createdAt: z.coerce.string(),
+  updatedAt: z.coerce.string(),
   versionHash: z.string(),
 });
 
 export const CommentDTOSchema = z.object({
   id: z.string().uuid(),
   text: z.string(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  deletedAt: z.string().nullable(),
+  createdAt: z.coerce.string(),
+  updatedAt: z.coerce.string(),
+  deletedAt: z.coerce.string().nullable(),
   versionHash: z.string(),
 });
 
-export const CodeSnippetDTOSchema = z.object({
+export const ExportedCommentSchema = z.object({
+  id: z.string().uuid(),
+  text: z.string(),
+  createdAt: z.coerce.string(),
+  updatedAt: z.coerce.string(),
+  deletedAt: z.coerce.string().nullable(),
+  versionHash: z.string(),
+  codeSnippetId: z.string(),
+});
+
+export const ExportedCodeSnippetSchema = z.object({
   id: z.string().uuid(),
   title: z.string(),
   code: z.string(),
   pinned: z.boolean(),
   isEditable: z.boolean(),
   currentLanguage: z.string(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
+  createdAt: z.coerce.string(),
+  updatedAt: z.coerce.string(),
   versionHash: z.string(),
-  deletedAt: z.string().nullable(),
+  deletedAt: z.coerce.string().nullable(),
   tags: z.array(z.string().uuid()),
-  comments: z.array(CommentDTOSchema),
+  comments: z.array(z.string().uuid()),
 });
 
 export const StorageShema = z.object({
-  codeSnippets: CodeSnippetDTOSchema.array(),
-  comments: CommentDTOSchema.array(),
+  codeSnippets: ExportedCodeSnippetSchema.array(),
+  comments: ExportedCommentSchema.array(),
   isSidebarCollapsed: z.boolean(),
   lastSyncDate: z.any().nullable(),
   tags: TagDTOSchema.array(),
@@ -54,4 +64,4 @@ export const AppMetaSchema = z.object({
 
 export type TagDTO = z.infer<typeof TagDTOSchema>;
 export type CommentDTO = z.infer<typeof CommentDTOSchema>;
-export type CodeSnippetDTO = z.infer<typeof CodeSnippetDTOSchema>;
+export type CodeSnippetDTO = z.infer<typeof ExportedCodeSnippetSchema>;
